ec1b55705d0ce2f033e85673399c03c75303c6a7
Kallyan Mombach <mombachkallyan@gmail.com>
2025-03-05 19:25:21 -0300

    commit2

--- Alunos/Kallyan Mombach/Aula02/Hello.java
+++ Alunos/Kallyan Mombach/Aula02/Hello.java
@@ -2,8 +2,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		HelloWorld();
 
 	}
-
+	public static void HelloWorld(){
+		System.out.println("Hello World!!!");
+	}
 }
