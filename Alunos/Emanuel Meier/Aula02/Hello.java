2836931440ea5eabdc07411566756918be752ec7
Emanuel Meier Martins <emanuelmmartins@gmail.com>
2025-03-06 20:17:20 -0300

    Adicionado Aula02

---
+++ Alunos/Emanuel Meier/Aula02/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        sayHelloInPortuguese();
+        sayHelloInSpanish();
+    }
+
+     public static void sayHelloInPortuguese() {
+        System.out.println("Olá Mundo");
+    }
+
+     public static void sayHelloInSpanish() {
+        System.out.println("Hola que tal");
+    }
+}
