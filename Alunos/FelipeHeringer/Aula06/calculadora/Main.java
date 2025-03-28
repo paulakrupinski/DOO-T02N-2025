62e0fd07f88064337b58af88ef08d22bc91d89aa
Felipe Heringer <heringerfelipe14@gmail.com>
2025-03-27 21:40:23 -0300

    Adicionado Calculadora da Gabrielinha na Aula 06

---
+++ Alunos/FelipeHeringer/Aula06/calculadora/Main.java
@@ -0,0 +1,9 @@
+package Alunos.FelipeHeringer.Aula06.calculadora;
+
+public class Main {
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        menu.menuPrincipal();
+
+    }
+}
