b16456a53876603be63318d9f69e517aa8b34dc2
Joao Guilherme Lemes Argenta <joaoargenta3@gmail.com>
2025-04-10 19:24:30 -0300

    Implementando Classes e herança

---
+++ Alunos/João Guilherme/Aula07/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+  public String nome;
+  public int idade;
+  public Endereco endereco;
+
+  public Cliente(String nome, int idade, Endereco endereco) {
+      this.nome = nome;
+      this.idade = idade;
+      this.endereco = endereco;
+  }
+}
---
+++ Alunos/João Guilherme/Aula07/Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    int id;
+    String nome;
+    String tipo;
+    double valor;
+
+    public Item(int id, String nome, String tipo, double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public void gerarDescricao() {
+        System.out.println("Item ID: " + id + " - Nome: " + nome + ", Tipo: " + tipo + ", Valor: R$" + valor);
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
