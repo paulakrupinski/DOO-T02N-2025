c80825f2c77959ba2eb968592638dabd9e69134b
Grazielli31 <graziellipereira46@gmail.com>
2025-04-10 21:18:07 -0300

    Add Aula06

---
+++ Alunos/Grazielli/Aula06/Vendedor.java
@@ -0,0 +1,38 @@
+public class Vendedor {
+    String nome;
+    int idade;
+    Loja loja;
+    String cidade;
+    String bairro;
+    String rua;
+    double salarioBase;
+    double[] salarioRecebido;
+
+    public Vendedor(String nome, int idade, Loja loja, String cidade, String bairro, String rua, double salarioBase,
+            double[] salarioRecebido) {
+        this.nome = nome;
+        this.idade = idade;
+        this.loja = loja;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.salarioBase = salarioBase;
+        this.salarioRecebido = salarioRecebido;
+    }
+
+    public void apresentarse() {
+        System.out.println("Nome: " + nome + " | Idade: " + idade + " | Loja: " + loja.nomeFantasia);
+    }
+
+    public double calcularMedia() {
+        double soma = 0;
+        for (double salario : salarioRecebido) {
+            soma += salario;
+        }
+        return soma / salarioRecebido.length;
+    }
+
+    public double calcularBonus() {
+        return salarioBase * 0.2;
+    }
+}
