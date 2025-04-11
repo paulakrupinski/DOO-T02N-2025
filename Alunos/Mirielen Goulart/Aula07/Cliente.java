4b136ac901dc7c29edf29ee1aa196ff6c252007a
mirigoulart <mirigoulartdossantos@gmail.com>
2025-04-10 22:14:47 -0300

    Aula 07

---
+++ Alunos/Mirielen Goulart/Aula07/Cliente.java
@@ -0,0 +1,58 @@
+
+public class Cliente {
+	
+	 String nome;
+	private int idade;
+	private Endereco endereco;
+	
+	public Cliente(String nome, int idade, Endereco endereco) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.endereco = endereco;
+	}
+	
+	
+	
+	 public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+
+
+	public void apresentarSe() {
+		 	System.out.println("=========== CLIENTE ===========");
+	        System.out.println("Nome: " + nome + ", Idade: " + idade);
+	        endereco.apresentarLogradouro();
+	    }
+}
---
+++ Alunos/Mirielen Goulart/Aula07/Vendedor.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+
+public class Vendedor extends Funcionario {
+
+	public Vendedor(String nome, int idade, String loja, Endereco endereco, double salarioBase,
+			ArrayList<Double> salarioRecebido) {
+		super(nome, idade, loja, endereco, salarioBase, salarioRecebido);
+	}
+	
+	public double calcularMedia() {
+        double soma = 0;
+        for (double salario : getSalarioRecebido()) {
+            soma += salario;
+        }
+        return getSalarioRecebido().isEmpty() ? 0 : soma / getSalarioRecebido().size();
+    }
+	
+	public void calcularBonus() {
+        System.out.println("Bônus: " + (getSalarioBase() * 0.2));
+    }
+	
+	public void apresentarse() {
+		System.out.println("========= VENDEDORES ============");
+		System.out.println("Nome: "+getNome()+ "Idade: " +getIdade()+ "Loja: "+getLoja());
+	}
+}
