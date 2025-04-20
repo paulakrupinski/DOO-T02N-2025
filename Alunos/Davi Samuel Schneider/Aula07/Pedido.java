8008d5d7f552805f74f9b26d59d0ec0faaf6d663
Davi Samuel Schneider <davvisamuels@gmail.com>
2025-04-20 20:19:25 -0300

    feat: adiciona aula07

---
+++ Alunos/Davi Samuel Schneider/Aula07/Pedido.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.util.Date;
+
+public class Pedido {
+    private String id;
+    private Date dataCriacao;
+    private Date dataPagamento;
+    private Date dataVencimentoReserva;
+    private Cliente cliente;
+    private Vendedor vendedor;
+    private Loja loja;
+    private Item[] itens;
+
+    public Pedido(String id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva, Cliente cliente, Vendedor vendedor, Item[] itens) {
+        this.id = id;
+        this.dataCriacao = dataCriacao;
+        this.dataPagamento = dataPagamento;
+        this.dataVencimentoReserva = dataVencimentoReserva;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.itens = itens;
+    }
+
+    public Pedido(String id, Date dataCriacao, Date dataPagamento, Date dataVencimentoReserva, Cliente cliente, Vendedor vendedor, Loja loja, Item[] itens) {
+        this.id = id;
+        this.dataCriacao = dataCriacao;
+        this.dataPagamento = dataPagamento;
+        this.dataVencimentoReserva = dataVencimentoReserva;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        this.loja = loja;
+        this.itens = itens;
+    }
+
+    public double valorTotal() {
+        if (itens == null) return 0;
+        double total = 0;
+        for (Item item : itens) {
+            total += item.getValor();
+        }
+        return total;
+    }
+
+    public void gerarDescricaoVenda() {
+        System.out.println("Pedido criado em: " + dataCriacao + " | Valor total: R$" + valorTotal());
+    }
+}
