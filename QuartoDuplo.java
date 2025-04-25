43be040ee77347e36f3b12531936b60f501a65d9
Mautc <mauriliotcoloda2006@gmail.com>
2025-04-24 21:57:40 -0300

    PROVA_1TRI HOTEL

---
+++ QuartoDuplo.java
@@ -0,0 +1,16 @@
+public class QuartoDuplo extends Quarto {
+
+    public QuartoDuplo(int num) {
+        super(2, num, 400);
+    }
+
+
+    public void setValor() {
+        super.setValor(400);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
