21f0624068782c2732f46b17758f78d2994e88b6
Charles Müller <62445331+charlesmuller194@users.noreply.github.com>
2025-05-22 23:59:39 -0300

    feat: implementação da calculadora com operações matemáticas e tratamento de exceções

---
+++ Alunos/Charles Müller/Aula 09/OperacaoSubtracao.java
@@ -0,0 +1,13 @@
+public class OperacaoSubtracao implements OperacaoMatematica {    
+    @Override
+    public double executar(double a, double b) throws CalculadoraException {
+        double resultado = a - b;
+        VerificadorMatematico.verificarOverflow(resultado);
+        return resultado;
+    }
+    
+    @Override
+    public String getSimbolo() {
+        return "-";
+    }
+}
